3470c8b69a04409df8f4d498cd5e4d019b12fb71
picasobriz <your.email@example.com>
2026-07-14 15:23:06 +0300

    2ndtrial

--- index.js
+++ index.js
@@ -1,47 +1,31 @@
 const bookStore = {
-    name: 'Flatbooks Technical Books',
-    books: [
-        {
-            id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-            author: 'Marjin Haverbeke',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
-        },
-        {
-            id:2,
-            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-            author: 'Jon Duckett',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-        },
-        {
-            id:3,
-            title: 'JavaScript: The Good Parts',
-            author: 'Douglas Crockford',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-        },
-        {
-            id:4,
-            title: 'JavaScript: The Definitive Guide',
-            author: 'David Flanagan',
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-            
-        },
-        {
-            id:5,
-            title: 'You Don’t Know JS',
-            author: 'Kyle Simpson',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-        },
-        {
-            id:6,
-            title: 'Cracking the Coding Interview',
-            author: 'Gayle Laakmann McDowell',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-            
-        }
-    ]
+  name: 'Flatbooks Technical Books',
+  books: [
+    { id:1, title: 'Eloquent JavaScript: A Modern Introduction to Programming', author: 'Marjin Haverbeke', imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg' },
+    { id:2, title: 'JavaScript & JQuery: Interactive Front-End Web Development', author: 'Jon Duckett', imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg' },
+    { id:3, title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg' },
+    { id:4, title: 'JavaScript: The Definitive Guide', author: 'David Flanagan', imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg" },
+    { id:5, title: 'You Don’t Know JS', author: 'Kyle Simpson', imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg' },
+    { id:6, title: 'Cracking the Coding Interview', author: 'Gayle Laakmann McDowell', imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg' }
+  ]
 }
 
-// Write your code here!
+// Write your code here
+let bookStoreTitle = document.getElementById('header');
+bookStoreTitle.textContent = bookStore.name;
 
+for(let book of bookStore.books){
+    let bookContainer = document.createElement('li');
+    let bookTitle = document.createElement('h3');
+    let bookAuthor = document.createElement('p');
+    let bookImage = document.createElement('img');
+
+    bookTitle.textContent = book.title
+    bookAuthor.textContent = book.author
+    bookImage.src = book.imageUrl
+
+    bookContainer.append(bookTitle,bookAuthor,bookImage);
+ 
+    let bookList = document.getElementById('book-list');
+    bookList.appendChild(bookContainer)
+};
